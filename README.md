1997e5bcbc041e400f1fa45047c30c6307aa7a4b
Marc Bitterli <nxt191@gmail.com>
2021-01-20 16:30:27 +0100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # react-multi-progress
+
+A simple, typed react progress bar that allowes multiple layers in different colors. [http://progress.bitter.li](Demo)
+
+## Installation
+
+Install with npm:
+
+- `npm install react-multi-progress --save`
+
+You can now import `react-multi-progress` as a normal package installed from npm like so:
+
+```
+import MultiProgress from 'react-multi-progress'
+...
+```
+
+You can also import the type definitions if you're using TypeScript like so:
+
+```
+import MultiProgress, { IMultiProgressProps } from 'react-multi-progress'
+...
+```
+
+## Available props
+
+| Attribute        |        Type         |  Default  | Description                                                                                              |
+| :--------------- | :-----------------: | :-------: | :------------------------------------------------------------------------------------------------------- |
+| backgroundColor  |      `string`       | `#ffffff` | Render a specific button type, styled by the .scss type list                                             |
+| border           |      `string`       |    ``     | Render a specific button size, styled by the .scss size list                                             |
+| elements         | `ProgressElement[]` |  `none`   | Overwrites the default container element renderer, useful for using it with react-router Link component. |
+| height           |      `number`       |   `10`    | Should render a disabled button                                                                          |
+| round            |       `bool`        |  `true`   | Should the button be visible                                                                             |
+| roundLastElement |       `bool`        |  `true`   | Should render the animated ripple effect                                                                 |
+| transitionTime   |      `number`       |   `0.6`   | Default click/press function                                                                             |
---
+++ src/index.tsx
@@ -0,0 +1,108 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import * as React from "react";
+import { css } from "glamor";
+
+interface ProgressElement {
+	value: number;
+	color: string;
+}
+
+export type IMultiProgressProps = {
+	backgroundColor?: string;
+	border?: string;
+	elements: ProgressElement[];
+	height?: number | string;
+	round?: boolean;
+	roundLastElement?: boolean;
+	transitionTime?: number;
+};
+const styles = {
+	progressContainer: (round: boolean, height: string | number, border: string) => {
+		const convertedHeight = typeof height === "string" ? height : height + "px";
+		return css({
+			width: "100%",
+			height: convertedHeight,
+			position: "relative",
+			border,
+			borderRadius: round ? "400px" : "0",
+			overflow: "hidden",
+		});
+	},
+	progressBackground: (backgroundColor: string) =>
+		css({
+			backgroundColor,
+			width: "100%",
+			height: "100%",
+			position: "absolute",
+			top: 0,
+			left: 0,
+		}),
+	progressElement: (
+		backgroundColor: string,
+		offset: number,
+		value: number,
+		transitionTime: number,
+		roundRight: boolean
+	) => {
+		const roundRightString = roundRight ? "40px 40px" : "0 0";
+		return css({
+			backgroundColor,
+			width: value + "%",
+			zIndex: 8,
+			height: "100%",
+			top: 0,
+			left: offset + "%",
+			position: "absolute",
+			transition:
+				"width " + transitionTime + "s ease-in-out, left " + transitionTime + "s ease-in-out",
+			borderRadius: "0 " + roundRightString + " 0",
+		});
+	},
+};
+
+const createElementArray = (
+	elements: ProgressElement[],
+	transitionTime: number,
+	roundLastElement: boolean
+) => {
+	let currentOffset = 0;
+	let newElements = [] as any[];
+
+	elements.forEach((element, i) => {
+		newElements.push(
+			<div
+				{...styles.progressElement(
+					element.color,
+					currentOffset,
+					element.value,
+					transitionTime,
+					i === elements.length - 1 && roundLastElement
+				)}
+				key={i}
+			/>
+		);
+		currentOffset += element.value;
+	});
+	return newElements;
+};
+
+const MultiProgress: React.FC<IMultiProgressProps> = ({
+	backgroundColor = "#ffffff",
+	border = "",
+	elements,
+	height = 10,
+	round = true,
+	roundLastElement = true,
+	transitionTime = 0.6,
+}) => {
+	return (
+		<div {...styles.progressContainer(round, height, border)}>
+			<div {...styles.progressBackground(backgroundColor)} />
+			{createElementArray(elements, transitionTime, roundLastElement).map((element, i) => (
+				<div key={i}>{element}</div>
+			))}
+		</div>
+	);
+};
+
+export default MultiProgress;
